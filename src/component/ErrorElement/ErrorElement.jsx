4d6ae657920a078bf748fa234674247576a466b6
coderpbt <yeasin800398@gmail.com>
2022-10-12 09:14:28 +0600

    errorpage create

--- src/component/ErrorElement/ErrorElement.jsx
+++ src/component/ErrorElement/ErrorElement.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ErrorElement = () => {
   return (
-    <div className='text-3xl font-semibold min-h-screen flex items-center justify-center text-center'>
-      <h3 className='text-white'>404 Not Found!!!</h3>
+    <div className='font-semibold min-h-screen flex items-center justify-center text-center'>
+      <h3 className='text-white text-4xl'>404 Not Found!!!</h3>
     </div>
   );
 };
